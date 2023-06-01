cd36b39cfad6daa7a6a0ce6107735892b089e0ec
Fabricio <fabricio.seb1@gmail.com>
2023-06-01 20:06:56 -0300

    imagens padrao por classificacao

--- src/screens/Result.js
+++ src/screens/Result.js
@@ -7,10 +7,19 @@ import {
   Spaces,
 } from "../shared/DesignTokens";
 import { Button } from "../components/Button";
-import PlantImg from "../images/plant-mockup.png";
 import ImageContext from "../context/ImageContext";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import formigacortadeira from "../images/pragas/formigacortadeira.png";
+import cochonilha from "../images/pragas/cochonilha.jpg";
+import acarorajado from "../images/pragas/acarorajado.jpeg";
+import coros from "../images/pragas/coros.jpg";
+import helicoverpaarmigera from "../images/pragas/helicoverpaarmigera.jpg";
+import lagartadocartucho from "../images/pragas/lagartadocartucho.jpg";
+import larvaminadora from "../images/pragas/larvaminadora.jpg";
+import moscabranca from "../images/pragas/mosca-branca.jpg";
+import percevejomarrom from "../images/pragas/percevejomarrom.jpg";
+import pulgoes from "../images/pragas/pulgoes.jpg";
 
 const ResultsBox = styled.div`
   box-sizing: border-box;
@@ -48,16 +57,45 @@ const PlantImage = styled.img`
 const Description = styled.p`
   padding: 0 0 0 25px;
 `;
+
 export function Result() {
   const { image } = useContext(ImageContext);
   let navigate = useNavigate();
+
+  const renderDefaultImg = (nome) => {
+    switch (nome) {
+      case "Formiga cortadeira":
+        return <PlantImage src={formigacortadeira} />;
+      case "Cochonilhas":
+        return <PlantImage src={cochonilha} />;
+      case "Pulgões":
+        return <PlantImage src={pulgoes} />;
+      case "Ácaro rajado":
+        return <PlantImage src={acarorajado} />;
+      case "Corós":
+        return <PlantImage src={coros} />;
+      case "Helicoverpa armigera":
+        return <PlantImage src={helicoverpaarmigera} />;
+      case "Lagarta-do-cartucho":
+        return <PlantImage src={lagartadocartucho} />;
+      case "Larva minadora":
+        return <PlantImage src={larvaminadora} />;
+      case "Mosca-branca":
+        return <PlantImage src={moscabranca} />;
+      case "Percevejo-marrom":
+        return <PlantImage src={percevejomarrom} />;
+
+      default:
+        break;
+    }
+  };
 
   return (
     <ResultsBox>
       <Logo />
       <h2>{image.label}</h2>
       <Flex>
-        <PlantImage src={PlantImg} />
+        {renderDefaultImg(image.label)}
         <Description>
           Uma breve descrição do que é a doença e algumas formas de tratá-la.
           Acho que a gente pode inclusive gerar ela com uma requisição para a
