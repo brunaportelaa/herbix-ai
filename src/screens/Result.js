e45873434a7609188aee0e67ab63a70835814452
Fabricio <fabricio.seb1@gmail.com>
2023-06-07 17:29:03 -0300

    adicionados textos padroes por categoria

--- src/screens/Result.js
+++ src/screens/Result.js
@@ -62,28 +62,58 @@ export function Result() {
   const { image } = useContext(ImageContext);
   let navigate = useNavigate();
 
-  const renderDefaultImg = (nome) => {
+  const renderDefault = (nome) => {
     switch (nome) {
       case "Formiga cortadeira":
-        return <PlantImage src={formigacortadeira} />;
+        return {
+          image: <PlantImage src={formigacortadeira} />,
+          text: "As formigas cortadeiras são insetos que cortam e transportam fragmentos de folhas para seus ninhos. Elas cultivam um fungo nos fragmentos cortados, que serve de alimento para a colônia. Esse comportamento pode prejudicar o crescimento das plantas afetadas.",
+        };
       case "Cochonilhas":
-        return <PlantImage src={cochonilha} />;
+        return {
+          image: <PlantImage src={cochonilha} />,
+          text: "A cochonilha é um inseto sugador que ataca plantas, formando carapaças brancas ou marrons em seus ramos e folhas. Ela se alimenta da seiva das plantas, enfraquecendo-as e podendo levar à morte das partes afetadas.",
+        };
       case "Pulgões":
-        return <PlantImage src={pulgoes} />;
+        return {
+          image: <PlantImage src={pulgoes} />,
+          text: "Os pulgões são pequenos insetos sugadores que se multiplicam rapidamente. Eles se alimentam da seiva das plantas, causando danos diretos, como deformações e amarelecimento das folhas. Além disso, eles também podem transmitir vírus às plantas, agravando os danos.",
+        };
       case "Ácaro rajado":
-        return <PlantImage src={acarorajado} />;
+        return {
+          image: <PlantImage src={acarorajado} />,
+          text: "É um tipo de ácaro que causa danos nas plantas, formando manchas avermelhadas nas folhas. Essas manchas são resultado da sucção de seiva pelas larvas do ácaro.",
+        };
       case "Corós":
-        return <PlantImage src={coros} />;
+        return {
+          image: <PlantImage src={coros} />,
+          text: "Os coros são doenças fúngicas que afetam as plantas, causando manchas necróticas nas folhas e frutos. Essas manchas podem se espalhar rapidamente, comprometendo a saúde e o desenvolvimento da planta.",
+        };
       case "Helicoverpa armigera":
-        return <PlantImage src={helicoverpaarmigera} />;
+        return {
+          image: <PlantImage src={helicoverpaarmigera} />,
+          text: "A helicoverpa armigera é uma espécie de lagarta considerada uma praga agrícola. Ela ataca diversas culturas, causando danos significativos às plantas, especialmente às folhas, flores e frutos, prejudicando a produção agrícola.",
+        };
       case "Lagarta-do-cartucho":
-        return <PlantImage src={lagartadocartucho} />;
+        return {
+          image: <PlantImage src={lagartadocartucho} />,
+          text: "A lagarta do cartucho é uma praga que ataca espigas de milho. Ela se alimenta dos grãos em desenvolvimento, causando danos severos à produção. É importante controlar essa lagarta para evitar perdas econômicas.",
+        };
       case "Larva minadora":
-        return <PlantImage src={larvaminadora} />;
+        return {
+          image: <PlantImage src={larvaminadora} />,
+          text: "Lavaminadoras são larvas de insetos que se alimentam do tecido foliar das plantas. Elas criam rastros minados nas folhas, deixando-as com aparência de enrugadas e com áreas transparentes. Esse tipo de dano pode afetar a fotossíntese e enfraquecer a planta.",
+        };
       case "Mosca-branca":
-        return <PlantImage src={moscabranca} />;
+        return {
+          image: <PlantImage src={moscabranca} />,
+          text: "A mosca branca é um pequeno inseto sugador que se alimenta da seiva das plantas. Além de enfraquecer as plantas, ela também pode transmitir vírus, levando a doenças e danos mais severos.",
+        };
       case "Percevejo-marrom":
-        return <PlantImage src={percevejomarrom} />;
+        return {
+          image: <PlantImage src={percevejomarrom} />,
+          text: "O percebo marrom é um inseto que se alimenta da seiva das plantas. Além de causar danos diretos, enfraquecendo a planta, ele também pode transmitir doenças às plantas, prejudicando seu crescimento e desenvolvimento.",
+        };
 
       default:
         break;
@@ -95,12 +125,8 @@ export function Result() {
       <Logo />
       <h2>{image.label}</h2>
       <Flex>
-        {renderDefaultImg(image.label)}
-        <Description>
-          Uma breve descrição do que é a doença e algumas formas de tratá-la.
-          Acho que a gente pode inclusive gerar ela com uma requisição para a
-          API do Chat GPT, se não for muito complexo.
-        </Description>
+        {renderDefault(image.label).image}
+        <Description>{renderDefault(image.label).text}</Description>
       </Flex>
       <Button onClick={() => navigate("/")}>Fazer outra pesquisa</Button>
     </ResultsBox>
